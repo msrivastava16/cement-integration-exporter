67bc2b2c6ef444fd530a322a1bb427c5e4b26756
Mansi Srivastava <srivastava.1989.mansi@gmail.com>
2025-08-07 11:35:13 +1000

    feat: Add GitHub Actions workflow

--- createPackageAndFlows.py
+++ createPackageAndFlows.py
@@ -1,48 +1,7 @@
 import requests
 import os
-import yaml
-import argparse
-import jinja2
-from jinja2 import Template
 import csv
 import base64
-
-
-def write_file(file_path, content):
-    lock_file_path = f"{file_path}.lock"
-    with open(lock_file_path, 'w') as f:
-        f.write("lock")
-    with open(file_path, 'wb') as f:
-        f.write(content)
-    os.remove(lock_file_path)
-
-#get tenant config
-def getConfig(args):
-    with open('config.yml', 'r') as file:
-        config = yaml.safe_load(file)
-
-    oauth_url = config["all"]["tenants"]["dev"]["oAuthUrl"]
-    client_id = config["all"]["tenants"]["dev"]["client_id"]
-    client_secret = config["all"]["tenants"]["dev"]["client_secret"]
-    base_url = config["all"]["tenants"]["dev"]["baseURL"]
-    package_url = config["all"]["tenants"]["dev"]["PackageURL"]
-    artifacts_url = config["all"]["tenants"]["dev"]["ArtifactsURL"]
-
-    if args.environment == 'dev':
-        # Do something for the dev environment
-        print("dev selected")
-    elif args.environment == 'stage':
-        # Do something for the stage environment
-        oauth_url = config["all"]["tenants"]["stage"]["oAuthUrl"]
-        client_id = config["all"]["tenants"]["stage"]["client_id"]
-        client_secret = config["all"]["tenants"]["stage"]["client_secret"]
-        base_url = config["all"]["tenants"]["stage"]["baseURL"]
-        package_url = config["all"]["tenants"]["stage"]["PackageURL"]
-        artifacts_url = config["all"]["tenants"]["stage"]["ArtifactsURL"]
-        print("stage selected")
-    else:
-        print("no such tenant")
-    return oauth_url, client_id, client_secret, base_url, package_url, artifacts_url
 
 # Get OAuth Token
 def getOAuthToken(oauth_url, client_id, client_secret):
@@ -60,11 +19,14 @@ def fetchCSRFToken(base_url, oauth_token):
     csrf_token = csrf_response.headers['x-csrf-token']
     return csrf_token
 
-parser = argparse.ArgumentParser(description='choose you tenant')
-parser.add_argument('environment', choices=[ 'dev', 'stage'], help='The tenant to use')
-args = parser.parse_args()
+oauth_url = os.environ.get("OAUTH_URL")
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
+base_url = os.environ.get("BASE_URL")
 
-oauth_url, client_id, client_secret, base_url, package_url, artifacts_url = getConfig(args)
+if not all([oauth_url, client_id, client_secret, base_url]):
+    print("Error: One or more environment variables are not set.")
+    exit(1)
 
 oauth_token = getOAuthToken(oauth_url, client_id, client_secret)
 csrf_token = fetchCSRFToken(base_url, oauth_token)
@@ -82,7 +44,7 @@ def post_integration_package(headers):
     print(url)
     response = requests.post(url, headers=headers, json=data)
     print(response.text)
-    if response.status_code == 200:
+    if response.status_code == 201:
         print("Integration package successfully created.")
     else:
         print("Error creating integration package. Status code:", response.status_code)
@@ -111,7 +73,7 @@ def post_integration_artifact(headers):
 
             response = requests.post(url, headers=headers, json=data)
 
-            if response.status_code == 200:
+            if response.status_code == 201:
                 print(f"Integration flow '{name}' successfully created.")
             else:
                 print(f"Error creating integration flow '{name}'. Status code:", response.status_code)
--- downloadiFlows.py
+++ downloadiFlows.py
@@ -1,7 +1,5 @@
 import requests
 import os
-import yaml
-import argparse
 import jinja2
 from jinja2 import Template
 import aiohttp
@@ -21,34 +19,6 @@ def create_package_dir(result):
     package_id = result['PackageId']
     package_dir_path = f"./Get_All_Packages/{package_id}/"
     os.makedirs(package_dir_path, exist_ok=True)
-
-#get tenant config
-def getConfig(args):
-    with open('config.yml', 'r') as file:
-        config = yaml.safe_load(file)
-
-    oauth_url = config["all"]["tenants"]["dev"]["oAuthUrl"]
-    client_id = config["all"]["tenants"]["dev"]["client_id"]
-    client_secret = config["all"]["tenants"]["dev"]["client_secret"]
-    base_url = config["all"]["tenants"]["dev"]["baseURL"]
-    package_url = config["all"]["tenants"]["dev"]["PackageURL"]
-    artifacts_url = config["all"]["tenants"]["dev"]["ArtifactsURL"]
-
-    if args.environment == 'dev':
-        # Do something for the dev environment
-        print("dev selected")
-    elif args.environment == 'stage':
-        # Do something for the stage environment
-        oauth_url = config["all"]["tenants"]["stage"]["oAuthUrl"]
-        client_id = config["all"]["tenants"]["stage"]["client_id"]
-        client_secret = config["all"]["tenants"]["stage"]["client_secret"]
-        base_url = config["all"]["tenants"]["stage"]["baseURL"]
-        package_url = config["all"]["tenants"]["stage"]["PackageURL"]
-        artifacts_url = config["all"]["tenants"]["stage"]["ArtifactsURL"]
-        print("stage selected")
-    else:
-        print("no such tenant")
-    return oauth_url, client_id, client_secret, base_url, package_url, artifacts_url
 
 # Get OAuth Token
 def getOAuthToken(oauth_url, client_id, client_secret):
@@ -101,11 +71,16 @@ async def download_package(package_id, package_url, artifacts_url, headers):
                     os.system(f"unzip -q -o {file_path} -d {iflow_dir_path}")
             print("completed:" + package_id)
 
-parser = argparse.ArgumentParser(description='choose you tenant')
-parser.add_argument('environment', choices=[ 'dev', 'stage'], help='The tenant to use')
-args = parser.parse_args()
+oauth_url = os.environ.get("OAUTH_URL")
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
+base_url = os.environ.get("BASE_URL")
+package_url = os.environ.get("PACKAGE_URL")
+artifacts_url = os.environ.get("ARTIFACTS_URL")
 
-oauth_url, client_id, client_secret, base_url, package_url, artifacts_url = getConfig(args)
+if not all([oauth_url, client_id, client_secret, base_url, package_url, artifacts_url]):
+    print("Error: One or more environment variables are not set.")
+    exit(1)
 
 oauth_token = getOAuthToken(oauth_url, client_id, client_secret)
 csrf_token = fetchCSRFToken(base_url, oauth_token)
@@ -126,13 +101,9 @@ async def main():
     }
     package_ids = await fetch_package_ids(base_url,headers)
 
-    # package_size = len(package_ids)
-    # index = 1
     for package_id in package_ids:
         tasks.append(asyncio.create_task(download_package(
             package_id, package_url, artifacts_url, headers)))
-        # print(f"triggered {index}/{package_size}")
-        # index = index + 1
 
     await asyncio.gather(*tasks)
 
